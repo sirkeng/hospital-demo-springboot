0101d6459820a9a55b1bab9db258800b44fb55f9
SirKenG <sirkeng.me@gmail.com>
2025-06-04 01:18:38 +0700

    Initial commit: Spring Boot base setup

---
+++ src/test/java/com/sirkeng/hospitaldemo/HospitaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkeng.hospitaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitaldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
